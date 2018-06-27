db5305553b235803a1cfafab73c0370bde3b1d30
mjtedder <mjtedder@gmail.com>
2018-06-26 21:15:57 -0400

    inserting dummy data

--- models/advertisement.js
+++ models/advertisement.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
 
   let Ad = sequelize.define("ad", {
-      title: Sequelize.STRING,
+      title: Sequelize.STRING, //validation needed
       category: Sequelize.STRING,
       body: Sequelize.TEXT,
       updated_at: Sequelize.DATE,
@@ -9,6 +9,21 @@ module.exports = (sequelize, DataTypes) => {
       public: Sequelize.BOOLEAN,
       created_at: Sequelize.DATE
   })
+
+  // TEST THIS!
+  let ad = Ad.build({
+      title: 'This is a title.',
+      category: 'I am a cateogry.',
+      body: 'I am a body',
+      updated_at: DATE,
+      taken: false,
+      public: true,
+      created_at: DATE
+    })
+
+    ad.save().catch(error => {
+      console.log('ERROR')
+    })
 
   Ad.associate = (models) => {
     Ad.hasOne(User)
