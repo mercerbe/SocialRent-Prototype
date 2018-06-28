6c0c5752c8dacf37b13ef49156331d99797e4f8c
Admin <mercerbe@gmail.com>
2018-06-28 16:58:41 -0400

    pages rendering, static routes to login/signup working

--- routes/view-routes.js
+++ routes/view-routes.js
@@ -6,17 +6,17 @@ module.exports = (app) => {
 
   //index aka maketplace page
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/index.handlebars'))
+    res.render('index')
   })
 
   //signup page
   app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/signup.handlebars'))
+    res.render('signup')
   })
 
   //login page
   app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/login.handlebars'))
+    res.render('login')
   })
 
 
