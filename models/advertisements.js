56dc3d144ffd2e7cd3621e57db35cd6bc9b9a01d
mjtedder <mjtedder@gmail.com>
2018-07-01 10:10:17 -0400

    sync with master

--- models/advertisements.js
+++ models/advertisements.js
@@ -3,25 +3,50 @@ let User = require('./user')
 module.exports = (Sequelize, DataTypes) => {
 
   let Ad = Sequelize.define("ad", {
-      title: DataTypes.STRING, //validation needed
-      category: DataTypes.STRING,
-      body: DataTypes.TEXT,
-      taken: DataTypes.BOOLEAN,
-      public: DataTypes.BOOLEAN,
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 100]
+        }
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 100]
+        }
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: [10, 280]
+        }
+      },
+      taken: {
+        type: DataTypes.BOOLEAN,
+        validate: {
+          isBoolean: (val) => {
+                             return (typeof(val)=='boolean')
+                                }
+        }
+      },
+      public: {
+        type: DataTypes.BOOLEAN,
+        validate: {
+          isBoolean: (val) => {
+                             return (typeof(val)=='boolean')
+                                }
+        }
+      },
+      payment: {
+        type: DataTypes.DECIMAL(5, 2),
+        validate: {
+          isDecimal: true
+        }
+      }
     })
-
-  //create
-  let ad = Ad.create({
-      title: 'This is a title.',
-      category: 'I am a cateogry.',
-      body: 'I am a body',
-      taken: false,
-      public: true,
-    })
-
-  //Ad.associate = (models) => {
-    //Ad.hasOne(User)
-  //}
 
 //association
   //need to identify and associate each ad with an owner/advertiser and a sharer/user...
@@ -41,7 +66,6 @@ module.exports = (Sequelize, DataTypes) => {
   //   })
   //
   // }
-
 
   return Ad
 }
