e90708bd3802252ca77224b1bfa9d68789bb71bd
RomeoKilo125 <romeokilo125@gmail.com>
2018-07-04 23:54:23 -0400

    began ad creation

--- public/js/advert.js
+++ public/js/advert.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+  let adTitleField = $('#adTitle')
+  let adCategoryField = $('#adCategory')
+  let adBodyField = $('#adBody')
+  let userCountField = $('#userCount')
+  let btnSubmit = $('#btnSubmit')
 
+  btnSubmit.on('click', event => {
+    event.preventDefault()
+
+    let adData = {
+      title: adTitleField.val().trim(),
+      category: adCategoryField.val().trim(),
+      body: adBodyField.val().trim(),
+      userCount: userCountField.val().trim(),
+      creatorID: 
+    }
+
+  })
 })
