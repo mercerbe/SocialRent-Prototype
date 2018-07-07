e25dd44b25c613237ace1b5d58d6f0850fbae24e
RomeoKilo125 <romeokilo125@gmail.com>
2018-07-06 22:34:59 -0400

    added bouncer function to redirect to login screen

--- public/js/user.js
+++ public/js/user.js
@@ -1,4 +1,27 @@
 //different ids for users/advertisers
+function bouncer() {
+  if (!document.cookie) {
+    alert("no cookie redirect")
+    window.location.href = '/login'
+    return
+  }
+
+  let url = window.location.toString()
+  let ident = url.match(/\/(\w+)s\/(\d+$)/)
+  let userRole = ident[1]
+  let userID = ident[2]
+
+  let cookieData = document.cookie.match(/id=(\d+); role=(\w+)/)
+  let cookieRole = cookieData[2]
+  let cookieID = cookieData[1]
+
+  if (userRole !== cookieRole || userID !== cookieID) {
+    window.location.href = '/login'
+    return
+  }
+}
+
+bouncer()
 
 $(document).ready(function() {
   let email = $('#email_input')
